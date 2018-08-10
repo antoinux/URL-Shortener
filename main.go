23d64b5c08ce6c2b818df033e54e59243ff89f57
Antoine Turquetil <daemonginger@Bibi.localdomain>
2018-08-10 20:12:35 +0200

    add: flags to use custom ports and log file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package restful
+package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,9 +10,16 @@ import (
 	"restful/server"
 )
 
-var logFileBase string = "/home/lc22073/go/src/restful/log/log_url_shortener"
+var (
+	logFileBase string
+	frontPort   int
+	restPort    int
+)
 
 func main() {
+	flag.StringVar(&logFileBase, "logFile", "/tmp/log_url_shortener", "The base full filename for logs.")
+	flag.IntVar(&frontPort, "frontPort", 80, "The front server port.")
+	flag.IntVar(&restPort, "restPort", 8080, "The RESTful API server port.")
 	logFile := logFileBase + time.Now().Local().Format("2006-01-02")
 	f, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
 	if err != nil {
@@ -22,5 +30,5 @@ func main() {
 	}
 	log.SetOutput(f)
 	server := server.NewURLServer(server.Routes)
-	server.Start(8080, 8081)
+	server.Start(frontPort, restPort)
 }
